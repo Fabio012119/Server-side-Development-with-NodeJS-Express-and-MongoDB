b7339a62162b1b9ae39ec49876bebf752613b2bf
Fabio Saraseli <fabiooosara@gmail.com>
2022-08-05 09:20:58 -0700

    Express REST API with MongoDB and Mongoose Part 1

--- nodeJs/conFusionServer/app.js
+++ nodeJs/conFusionServer/app.js
@@ -11,6 +11,16 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
 
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://127.0.0.1:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
 
 
 
